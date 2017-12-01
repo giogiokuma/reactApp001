307cb75c0400c9b2620534f2028d388adc133903
Misako Ohata <ohata@raijin.com>
2017-12-01 10:34:32 +0900

    add form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
+
+
 import './App.css';
-import Validation from './Validation/Validation';
+import ContactList from './ContactList/ContactList'; 
+import CommentForm from './CommentForm/CommentForm'; 
 class App extends Component {
   state = {
-    userout: ''
+    name: '',
+    value: '',
   }
   textChangeHandler = (event) => {
     this.setState({
-      userout: event.target.value
+      name: event.target.value
     })
   }
-
+  handleChange = (event) => {
+    this.setState({
+      value:event.target.value
+    })
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
-    const style = {
-      display:'inline-block',
-      padding:'16px',
-      textAlign:'center',
-      margin:'16px',
-      border:'1px solid black'
-    };
+
     return (
       <div className="App">
+      <input type="text" onChange={this.textChangeHandler} />
+      <ContactList str={this.state.name} />
 
-        <input type="text"
-          onChange={this.textChangeHandler}
-          value={this.state.userout} /> 
-        <p>{this.state.userout}</p>
-        <Validation inputLength={this.state.userout.length} />
+      {/*<input type="text" onChange={} />*/}
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name: <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submmit" />
+      </form>
       </div>
+      
     );
   }
 }
---
+++ src/ContactList/ContactList.js
@@ -0,0 +1,14 @@
+
+import React from 'react'; 
+
+const contact = (props) => {
+  
+  return (
+    <div>
+      <p>{props.str}</p>
+      
+    </div>
+  )  
+}
+
+export default contact;
